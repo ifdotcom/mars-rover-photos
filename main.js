875378d968b16cadae2af006e72bb0d67824f79b
Fernanda Vidal <ifervb@gmail.com>
2023-02-12 21:55:40 -0600

    ADD: dynamic data

--- main.js
+++ main.js
@@ -5,26 +5,42 @@
 const API =
   "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=100&api_key=I9lXp6h9syLnSmguir3VNvWZlHPXfhUShKSPUdv8";
 
-const fetchData = async (urlApi) => {
-  const response = await fetch(urlApi);
-  const data = await response.json();
-  return data;
-};
+let btn = document.getElementById("btn"),
+  imgCard = document.querySelector(".card__image"),
+  titleCard = document.querySelector(".titile--card-text"),
+  martianSolValue = document.querySelector(".martianSolValue"),
+  roverNameValue = document.querySelector(".roverNameValue"),
+  cameraNameValue = document.querySelector(".cameraNameValue");
+// hacer peticion cuando se da click al boton
 
-const getElements = async (urlApi) => {
-  try {
-    const data = await fetchData(urlApi);
-    const photos = data.photos[0];
-    console.log(photos);
-    console.log(photos.id);
-  //  photos.forEach(element => {
-  //   console.log(element.id)
-  //  });
-  
-  } catch (error) {
-    console.log(error);
+btn.addEventListener("click", () => {
+  const fetchData = async (urlApi) => {
+    const response = await fetch(urlApi);
+    const data = await response.json();
+    return data;
+  };
+
+  const getElements = async (urlApi) => {
+    try {
+      const data = await fetchData(urlApi);
+      const photos = data.photos[getRandomInt(400)];
+      const roverData = photos.rover;
+      const cameraData = photos.camera;
+      //  modificar el DOM
+      imgCard.setAttribute("src", `${photos.img_src}`);
+      titleCard.innerHTML = `${cameraData.full_name}`;
+      martianSolValue.innerHTML = `${photos.sol}`;
+      roverNameValue.innerHTML = `${roverData.name}`;
+      cameraNameValue.innerHTML = `${cameraData.name}`;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  getElements(API);
+
+  // numero random
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
   }
-};
-
-getElements(API);
-
+});
