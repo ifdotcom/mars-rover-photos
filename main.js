92c6b8bf9d710975b44ed129dce7bce811a4f97e
Fernanda Vidal <ifervb@gmail.com>
2023-02-12 19:07:46 -0600

    SAVE: data API

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+//# IDEAS:
+//el número de sol puede ser variable:  hacer un random para cualquier sol
+//solo se requiere un solo id: hacer un random para mostrar una sola foto  (1-400)
+
 const API =
   "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=100&api_key=I9lXp6h9syLnSmguir3VNvWZlHPXfhUShKSPUdv8";
 
@@ -9,9 +13,14 @@ const fetchData = async (urlApi) => {
 
 const getElements = async (urlApi) => {
   try {
-    const photos = await fetchData(API);
+    const data = await fetchData(urlApi);
+    const photos = data.photos[0];
     console.log(photos);
-   displayData(photos)
+    console.log(photos.id);
+  //  photos.forEach(element => {
+  //   console.log(element.id)
+  //  });
+  
   } catch (error) {
     console.log(error);
   }
@@ -19,6 +28,3 @@ const getElements = async (urlApi) => {
 
 getElements(API);
 
-const displayData= data => {
-
-}
